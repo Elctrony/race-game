b5dce16660af2e833a0cd0e7bd7dd3786de1dc0f
Elctrony <48838991+Elctrony@users.noreply.github.com>
2023-11-29 02:21:04 +0200

    generateRandmon

--- macros.asm
+++ macros.asm
@@ -13,3 +13,14 @@ ScrollDown macro
      int 10h
 endm 
 
+GenerteRnd macro
+    MOV AH, 00h      
+    INT 1AH            
+    mov  ax, dx
+    xor  dx, dx
+    mov  cx, 10    
+    div  cx      
+    add  dl, '0'  
+    mov ah, 2h   
+    int 21h   
+endm
